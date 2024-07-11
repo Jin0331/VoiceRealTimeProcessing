8635ff3bf7d18262f72d92c75208f02ceb344410
Jin0331 <sempre813@naver.com>
2024-07-11 13:19:38 +0900

    Initial Commit

---
+++ VoiceRealTimeProcessing/VoiceRealTimeProcessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceRealTimeProcessingApp.swift
+//  VoiceRealTimeProcessing
+//
+//  Created by JinwooLee on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct VoiceRealTimeProcessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
